9cc5ee4455d1de598c9e95594f95bcbfb7f29483
Nekktor <Sok12Nik@yandex.ru>
2026-04-02 20:56:17 +0300

    Вывод команды select - список словарей

--- Database/models/chats.py
+++ Database/models/chats.py
@@ -10,7 +10,7 @@ class Chats(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(60), nullable=False)
     avatar_url = Column(Text)
-    date_created = Column(Date, server_default=func.current_date())
+    date_created = Column(String, server_default=func.current_date())
 
     participants = relationship("Participants", back_populates="chat", cascade="all, delete-orphan")
     users = relationship("Users", secondary="participants", back_populates="chats", overlaps="participants")
